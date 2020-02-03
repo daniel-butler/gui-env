fdb7a4f797cfea15b5b779058279d6b3e228fff3
Daniel Butler <daniel@dbutlercpa.com>
2020-02-02 20:33:44 -0500

    Updating setup tools

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-from distutils import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+setuptools.setup(
+    name="Guide"
+    version="0.0.1",
+    author="Daniel Butler",
+    author_email="dabutler89@gmail.com",
+    description="Graphical user interface for dot env files. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daniel-butler/guide",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
